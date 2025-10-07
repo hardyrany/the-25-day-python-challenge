d282a4a90677d679e8db7a48e25cd1ca5fc44a0f
hardyrany <hardymtf@gmail.com>
2025-10-07 06:36:27 -0100

    Started learnig Python, 25 Day Python Challenge

---
+++ Basics (Day 1)/02_comments.py
@@ -0,0 +1,8 @@
+# Get  the user's current time
+print("The time is: 6am") # Get  the user's current time
+
+# Print a string to the console
+print("Hello, World!")
+
+# print("Hello, World!")
+print("Hello, Bob!")
---
+++ Basics (Day 1)/03_variables.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  6 22:34:48 2025
+
+@author: HardyR
+"""
+
+name = "Bob"
+print("Hello, " + name + "!")
+
+# Non snake_case
+# 1name = "Bob"
+# my name = "Bob"
+# my-name = "Bob"
+# var@$name = "Bob"
+# for = "Bob"
